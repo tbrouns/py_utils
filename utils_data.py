c8330167b48917d4b098edc3236605b090934376
tbrouns <tbrouns>
2023-02-24 16:55:24 +0000

    time utils + json

--- utils_data.py
+++ utils_data.py
@@ -31,3 +31,16 @@ def load_ini(ini_path):
     config = configparser.ConfigParser()
     config.read(ini_path)
     return config
+
+
+def load_json(json_path):
+    if os.path.isfile(json_path):
+        with open(json_path, "r") as json_file:
+            return json.load(json_file)
+    else:
+        return None
+
+
+def save_json(json_dict, output_path):
+    with open(output_path, "w", encoding="utf-8") as file:
+        json.dump(json_dict, file, ensure_ascii=False, indent=4)
---
+++ utils_time.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+def get_current_date():
+    today = date.today()
+    return today.strftime("%Y-%m-%d")
